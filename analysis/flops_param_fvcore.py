449fdbb2045277d07031a6e518e6bf66d3a74821
liboyun <liboyun.gm@gmail.com>
2025-03-21 15:40:38 +0800

    update options for lightSR and fvcore

--- analysis/flops_param_fvcore.py
+++ analysis/flops_param_fvcore.py
@@ -4,9 +4,8 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.append(parent_dir)
-from model_zoo.mambaIR import buildMambaIR_light, buildMambaIR
-from model_zoo.mair import buildMaIR_light, buildMaIR, buildMaIR_tiny
-# from model_zoo.swinIR import buildSwinIR_light
+from model_zoo.mambaIR import buildMambaIR_Small, buildMambaIR
+from model_zoo.mair import buildMaIR_Small, buildMaIR, buildMaIR_Tiny
 
 from analysis.utils_fvcore import FLOPs
 fvcore_flop_count = FLOPs.fvcore_flop_count
@@ -16,9 +15,9 @@ if __name__ == '__main__':
     H=720
     W=1280
     scale=2
-    # init_model = buildMambaIR_light(upscale=scale).to(device)
-    # init_model = buildMaIR_light(upscale=scale).to(device)
-    init_model = buildMaIR_tiny(upscale=scale).to(device)
+    # init_model = buildMambaIR_Small(upscale=scale).to(device)
+    init_model = buildMaIR_Small(upscale=scale).to(device)
+    # init_model = buildMaIR_Tiny(upscale=scale).to(device)
     # init_model = buildMaIR(upscale=scale).to(device)
     # init_model = buildMambaIR(upscale=scale).to(device)
     with torch.no_grad():
---
+++ ckpt/readme.md
@@ -0,0 +1,1 @@
+The checkpoint can be downloaded from [here](https://drive.google.com/drive/folders/1YYmIVTyynLg-Kfu-mviq24WdVkJu-S3M?usp=sharing).
